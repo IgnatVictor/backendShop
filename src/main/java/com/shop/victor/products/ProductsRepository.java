c8c1a4be0efaf44e169d7ff0fdd5501a7d5ed98d
ignatvictor <meerr67@gmail.com>
2022-01-25 15:13:42 +0200

    search2"

--- src/main/java/com/shop/victor/products/ProductsRepository.java
+++ src/main/java/com/shop/victor/products/ProductsRepository.java
@@ -13,7 +13,7 @@ public interface ProductsRepository extends JpaRepository<Products, Long> {
 
       Page<Products> findAllByCategory(Pageable var1,String category);
 
-      Page<Products> findByDescriptionContainsAndNameContains(Pageable var1,String search);
+      Page<Products> findByDescriptionOrNameContains(Pageable var1,String search);
 
 
 }
