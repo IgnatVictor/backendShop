a76291ecda5e822b435a8a8e56e972e1a7ec4bc5
ignatvictor <meerr67@gmail.com>
2022-01-26 14:25:12 +0200

    data

--- src/main/java/com/shop/victor/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/shop/victor/SpringBootSecurityJwtApplication.java
@@ -1,8 +1,11 @@
 package com.shop.victor;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
+@EnableJdbcHttpSession
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
 
